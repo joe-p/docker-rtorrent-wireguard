a263beaf67ef2cc1b3d973c7f969807820238b29
Filipe Santos <filipe@not.sh>
2020-11-28 21:26:34 +1300

    Update Rutorrent default config

--- root/defaults/rutorrent-conf/config.php
+++ root/defaults/rutorrent-conf/config.php
@@ -2,10 +2,17 @@
 	// configuration parameters
 
 	// for snoopy client
-	@define('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0', true);
+	@define('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36', true);
 	@define('HTTP_TIME_OUT', 30, true);	// in seconds
 	@define('HTTP_USE_GZIP', true, true);
 	$httpIP = null;				// IP string. Or null for any.
+	$httpProxy = array
+	(
+		'use' 	=> false,
+		'proto'	=> 'http',		// 'http' or 'https'
+		'host'	=> 'PROXY_HOST_HERE',
+		'port'	=> 3128
+	);
 
 	@define('RPC_TIME_OUT', 5, true);	// in seconds
 
@@ -30,8 +37,8 @@
 	// $scgi_port = 5000;
 	// $scgi_host = "127.0.0.1";
 
-	// For web->rtorrent link through unix domain socket
-	// (scgi_local in rtorrent conf file), change variables
+	// For web->rtorrent link through unix domain socket 
+	// (scgi_local in rtorrent conf file), change variables 
 	// above to something like this:
 	//
 	$scgi_port = 0;
@@ -40,13 +47,13 @@
 	$XMLRPCMountPoint = "/RPC2";		// DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
 
 	$pathToExternals = array(
-		"php" 	=> '/usr/bin/php7',			// Something like /usr/bin/php. If empty, will be found in PATH.
-		"pgrep" => '/usr/bin/pgrep',			// Something like /usr/bin/pgrep. If empty, will be found in PATH.
-		"python" => '/usr/bin/python3',			// Something like /usr/bin/python3. If empty, will be found in PATH.
-		"curl"	=> '/usr/bin/curl',			// Something like /usr/bin/curl. If empty, will be found in PATH.
-		"gzip"	=> '/usr/bin/gzip',			// Something like /usr/bin/gzip. If empty, will be found in PATH.
-		"id"	=> '/usr/bin/id',			// Something like /usr/bin/id. If empty, will be found in PATH.
-		"stat"	=> '/bin/stat',			// Something like /usr/bin/stat. If empty, will be found in PATH.
+		"php" 	=> '',			// Something like /usr/bin/php. If empty, will be found in PATH.
+		"pgrep" => '',			// Something like /usr/bin/pgrep. If empty, will be found in PATH.
+		"python" => '',			// Something like /usr/bin/python3. If empty, will be found in PATH.
+		"curl"	=> '',			// Something like /usr/bin/curl. If empty, will be found in PATH.
+		"gzip"	=> '',			// Something like /usr/bin/gzip. If empty, will be found in PATH.
+		"id"	=> '',			// Something like /usr/bin/id. If empty, will be found in PATH.
+		"stat"	=> '',			// Something like /usr/bin/stat. If empty, will be found in PATH.
 	);
 
 	$localhosts = array( 			// list of local interfaces
@@ -61,6 +68,6 @@
 
 	$tempDirectory = '/config/rutorrent/profiles/tmp/';			// Temp directory. Absolute path with trail slash. If null, then autodetect will be used.
 
-	$canUseXSendFile = true;		// Use X-Sendfile feature if it exist
+	$canUseXSendFile = true;		// If true then use X-Sendfile feature if it exist
 
 	$locale = "UTF8";
